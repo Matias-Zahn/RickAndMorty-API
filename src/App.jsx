c86f0ce03b3b3e79edc2782ad594fe9f63271eae
Matias-Zahn <zahnmatias14@gmail.com>
2023-10-09 20:48:19 -0300

    B

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import getRandomNumber from "./utils/random"
-import Navbar from "./components/Navbar"
-import Residents from "./components/Residents"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import getRandomNumber from "./utils/random";
+import Navbar from "./components/Navbar";
+import Residents from "./components/Residents";
+import Loader from "./components/Loader";
 
 function App() {
-
-  const [location, setLocation] = useState(null)
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/location/${getRandomNumber(126)}`)
-      .then(( { data } ) => setLocation(data))
-      .catch(( error ) => console.log(error))
-  }, [])
+    axios
+      .get(`https://rickandmortyapi.com/api/location/${getRandomNumber(126)}`)
+      .then(({ data }) => setLocation(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <main className="min-h-full  text-white font-['Fira_Code']">
-      <section className="bg-[url(/bg/bg_header.svg)] bg-center bg-cover grid place-items-center ">
-
-          <div className="relative flex justify-center p-4">
-            <img src="/bg/portal.svg" alt="" />
-            <div className="absolute bottom-[60%]  translate-y-[50%]">
-              <img className="h-auto w-auto" src="/bg/logo.svg" alt="" />
+      {location === null ? (
+        <Loader />
+      ) : (
+        <>
+          <section className="bg-[url(/bg/bg_header.svg)] bg-center bg-cover grid place-items-center ">
+            <div className="relative flex justify-center ">
+              <img className="animate-pulse" src="/bg/portal.svg" alt="" />
+              <div className="absolute bottom-[60%]  translate-y-[50%]">
+                <img className="h-auto w-auto" src="/bg/logo.svg" alt="" />
+              </div>
             </div>
-          </div>
 
-        <Navbar location={location} setLocation={setLocation}/>
+            <Navbar location={location} setLocation={setLocation} />
+          </section>
 
-      </section>
-
-      <section className="bg-[url(/bg/background.svg)] min-h-screen bg-center bg-cover">
-
-        <Residents residents={location?.residents ?? []} />
-
-      </section>
+          <section className="bg-[url(/bg/background.svg)] min-h-screen bg-center bg-cover">
+            <Residents residents={location?.residents ?? []} />
+          </section>
+        </>
+      )}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+function Loader() {
+  return (
+    <div className="bg-[url(/bg/bg_header.svg)] bg-center bg-cover min-h-screen min-w-full flex items-center justify-center">
+        <h1 className="text-white text-9xl">
+          cargando
+        </h1>
+    </div>
+  )
+}
+export default Loader
