9152a640351e08f43d54759a000c4e84b3468750
Matias-Zahn <zahnmatias14@gmail.com>
2023-10-09 20:54:34 -0300

    C

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
     <div className="bg-[url(/bg/bg_header.svg)] bg-center bg-cover min-h-screen min-w-full flex items-center justify-center">
-        <div className="animate-spin rounded-full h-20 w-h-20 border-t-2 border-b-2 border-gray-900">
+        <div className="animate-spin rounded-full h-20 w-h-20 border-t-2 border-b-2 border-green-500">
         </div>
     </div>
   )
