af192c3559ab5c77bff159f277cd953161df0384
Matias-Zahn <zahnmatias14@gmail.com>
2023-10-09 21:58:32 -0300

    Finish

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -5,10 +5,10 @@ function SearchResult({resultSearch, setLocation}) {
   }
 
   return (
-    <div className="flex flex-col  items-center h-[200px] bg-transparent overflow-hidden overflow-y-scroll">
+    <div className="flex flex-col items-center h-[200px] bg-transparent overflow-hidden overflow-y-auto md:w-[500px] ">
         {
           resultSearch.map((result) => (
-            <div key={result.id} className="p-4 mt-1 hover:bg-green-500 transition-colors rounded-md cursor-pointer w-full text-center">
+            <div key={result.id} className="p-4  mt-1 hover:bg-green-500 w-full  transition-colors rounded-md cursor-pointer  text-center">
               <h4 onClick={() => handleChangedDimension(result)}>{result.name}</h4>
             </div>
           ))
